25fcdbaf6ae335dc94c6f70049d81314f25f8bd6
Shunsuke Kobayashi <shunsuke9434340942@g.ecc.u-tokyo.ac.jp>
2019-04-29 19:03:52 +0900

    識別に用いる分散の設定の変更

--- pattern-recognition/cov_diag.py
+++ pattern-recognition/cov_diag.py
@@ -3,6 +3,19 @@ import numpy.linalg as LA
 import matplotlib.pyplot as plt
 
 d=2; n=500
+# mean
+m1=np.array([0.,2.])[:,np.newaxis]
+m2=np.array([3.,0.])[:,np.newaxis]
+# variance
+g_cov1=np.array([[1.3, 1.2],[1.2, 1.7]])
+g_cov2=np.array([[1.4, -0.18],[-0.18, 1.4]])
+# probability
+p1=0.3
+p2=1-p1
+# data constellation
+x1=np.random.multivariate_normal(m1.reshape(m1.shape[0]), g_cov1, n).T
+x2=np.random.multivariate_normal(m2.reshape(m2.shape[0]), g_cov2, n).T
+
 
 def gausscontour(c,m,xx,yy):
     xt=xx-m[0,0]
@@ -18,15 +31,6 @@ def recognition(x, mu, sigma, p):
     +(-d/2.*np.log(2*np.pi)-1./2.*np.log(LA.norm(sigma, ord=1))+np.log(p))*np.ones(len(x.T))
 
 def classification(cov1, cov2, name):
-    # mean
-    m1=np.array([0.,2.])[:,np.newaxis]
-    m2=np.array([3.,0.])[:,np.newaxis]
-    # probability
-    p1=0.3
-    p2=1-p1
-    # data constellation
-    x1=np.random.randn(d, n)+m1.dot(np.ones([1,n]))
-    x2=np.random.randn(d, n)+m2.dot(np.ones([1,n]))
     w=m1-m2
 
     # recognition function ここを一般化するところから
@@ -56,17 +60,18 @@ def classification(cov1, cov2, name):
     pp=np.reshape(pp, xx.shape)
     cs=plt.contour(xx, yy, pp, cmap='hsv') # 識別線
     plt.clabel(cs)
-    plt.savefig(name+'cov_diag.eps')
-    # plt.show()
+    # plt.savefig(name+'cov_diag.eps')
+    plt.show()
 
 if __name__ == '__main__':
     sigma = 1.5
     # variance-covariance matrix
-    cov1=sigma**2*np.eye(2)
+    cov1=np.diag(np.cov(np.hstack([x1,x2])))*np.eye(d)
     cov2=cov1
     classification(cov1, cov2, "1")
-    cov1=np.array([[3.3, 1.2],[1.2, 1.7]])
+    cov1=np.cov(np.hstack([x1,x2]))
     cov2=cov1
     classification(cov1, cov2, "2")
-    cov2=np.array([[0.44, -0.18],[-0.18, 0.43]])
+    cov1=np.cov(x1)
+    cov2=np.cov(x2)
     classification(cov1, cov2, "3")
